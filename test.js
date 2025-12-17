3b83a30841100c994f1c7cad5a5538340d0d3862
laxmareddydev <laxmareddy.sub@gmail.com>
2025-12-17 15:00:12 +0530

    updated test.js file

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-const assert=require("assert")
-const {add}=require("/index")
-assert.strictEqual(add(2,3), 5)
-console.log("All tests are passed")
+const assert = require("assert");
+const { add } = require("./index");   // 👈 correct
+
+assert.strictEqual(add(2, 3), 5);
+console.log("All tests passed");
+
 
